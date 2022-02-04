8d403ad4a752e054297a7ca6ed6257dcf9027847
Abbas Khan <78753275+Abbas-glitch@users.noreply.github.com>
2022-02-04 18:48:22 +0430

    Add files via upload

---
+++ Building a DAO/hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x17731011C9A1C2b5f24b2fE7a3ac9dC0692fAC4d";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
